e9456014621023bbd4b228b95ab2c05b1e66af2e
gabriel-dp <gabriel.meira.2004@gmail.com>
2023-06-21 18:12:15 -0300

    :wrench: Remove id attribute from IdException

--- trabalho-pratico-2/JAVAutomotivo2/controller/IdException.java
+++ trabalho-pratico-2/JAVAutomotivo2/controller/IdException.java
@@ -2,15 +2,8 @@ package controller;
 
 public class IdException extends Exception {
 
-    private int id;
-
     public IdException(int id) {
         super(String.format("Id not found (%d)", id));
-        this.id = id;
-    }
-
-    public int getId() {
-        return this.id;
     }
 
 }
